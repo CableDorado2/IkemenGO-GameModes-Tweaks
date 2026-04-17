60ff7fb79977221d661ba706e0e6f94c2dd6708f
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-04-17 19:34:30 -0400

    Add files via upload

---
+++ external/mods/events/sample/event2.lua
@@ -0,0 +1,18 @@
+--[[EVENT EXAMPLE
+For all paramvalues available, check the Engine Wiki:
+https://github.com/ikemen-engine/Ikemen-GO/wiki/Lua#launchfight
+]]
+
+if gameMode() ~= nil and gameMode() ~= "" then --To avoid execute this script as module when boot the engine
+	if matchNo() == 1 then
+		if not launchFight{
+			p2char = {"kfm720"},
+			p2numchars = 1,
+			p2teammode = "single",
+			p2rounds = 1,
+			time = 30,
+			stage = "stages/stage3d.def",
+		} then return end
+	end
+	setMatchNo(-1)
+end
