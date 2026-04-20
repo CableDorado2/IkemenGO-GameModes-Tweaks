48ac7baa321857576e673c5d4f76a4d67ed1bf5f
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-04-20 19:32:15 -0400

    Nightly Update

---
+++ external/mods/vs100kumite/README.md
@@ -0,0 +1,43 @@
+# VS 100 Kumite Mode Tweaks (I.K.E.M.E.N. GO Module)
+
+Add the following Personal Improvements for [K4thos VS 100 Kumite Module](https://github.com/ikemen-engine/modules/tree/main/external/mods/vs100kumite) to make it more faithful to   
+Street Fighter Alpha 3 which it is based on:
+
+- Enables the "Here Comes a New Challenger" Intermission
+- MatchNo now is displayed in VS Screen
+- Stage Select Disabled
+- Adds Co-Op and Netplay Variant
+
+> [!NOTE]
+> - **This module overwrites the [original](https://github.com/ikemen-engine/modules/tree/main/external/mods/vs100kumite) VS 100 Kumite module**, so if you're using it, make a backup.
+> 
+> This mode is detectable by [GameMode](https://github.com/ikemen-engine/Ikemen-GO/wiki/Triggers-(new)#gamemode) trigger as:
+>- vs100kumite
+>- vs100kumitecoop
+>- netplayvs100kumitecoop
+
+## Installation
+
+### 1) Install the module
+Copy the entire `vs100kumite` directory into:
+`Ikemen_GO/external/mods/`
+
+Ikemen GO will load the module automatically on startup.
+
+### 2) Add the menu item to your screenpack
+Add the below entry to your main `system.def` file, under **`[Title Info]`**, alongside other `menu.itemname.*` entries. Place it where you want it to be grouped/ordered in the menu (grouping rules: https://github.com/ikemen-engine/Ikemen-GO/wiki/Screenpack-features#menus).
+Without this, the mode won't show up in main menu.
+
+```ini
+[Title Info]
+;VS 100 Kumite Modes
+menu.itemname.vs100kumite = "VS 100 KUMITE"
+menu.itemname.vs100kumitecoop = "VS 100 KUMITE CO-OP"
+menu.itemname.server.netplayvs100kumitecoop = "VS 100 KUMITE CO-OP"
+````
+
+## Screenpack / `system.def` defaults
+
+The module includes its own `system.def` (next to the script) with **default values for the 720p ikemen1 motif**.
+
+You can leave these defaults in the module or overwrite as your needs.
---
+++ external/mods/vs100kumite/vs100kumite.lua
@@ -0,0 +1,152 @@
+--[[	   					       	 VS 100 KUMITE MODULE
+=======================================================================================================
+Original Author: K4thos | Edited By: Cable Dorado 2 (CD2)
+Tested on: I.K.E.M.E.N. GO Engine (Nightly Build - 2026.04.20)
+Description: Defeat as many opponents as possible across 100 consecutive matches.
+Based on Street Fighter Alpha 3.
+
+This mode is detectable by GameMode trigger as vs100kumite, vs100kumitecoop and netplayvs100kumitecoop
+=======================================================================================================
+CD2's Tweaks:
+- MatchNo now is displayed in VS Screen
+- Stage Select Disabled
+- Enables the "Here Comes a New Challenger" Intermission
+- Adds Co-Op and Netplay Variant
+=======================================================================================================
+]]
+
+--;===========================================================
+--; main.lua
+--;===========================================================
+--[[
+main.t_itemname is a table storing functions with general game mode
+configuration (usually ending with start.f_selectMode function call).
+]]
+local function f_commonCfg()
+	main.aiRamp = false
+	main.charparam.ai = true
+	main.charparam.music = true
+	main.charparam.single = true
+	main.charparam.stage = true
+	main.charparam.time = true
+	
+	main.dropDefeated = false --defeated members are not removed from team
+	main.elimination = false --single lose doesn't stop further lua execution
+	main.forceRosterSize = true --roster size enforced even if there are not enough characters to fill it
+	main.rotationChars = true
+	main.exitSelect = true
+	--main.stageMenu = true --Enable Stage Select
+	main.makeRoster = true
+	--main.roundTime = 99
+	
+	--main.persistLife = true --life maintained after match
+	--main.persistMusic = true --don't stop the previous music at the start of the match.
+	--main.persistRounds = true --lifebar uses consecutive wins for round numbers
+	
+	main.motif.vsscreen = true
+	main.motif.vsmatchno = true
+	main.motif.continuescreen = false --no continue screen after lose the match
+	main.motif.hiscore = true
+	main.motif.losescreen = false --no lose screen after lose the match
+	--main.motif.winscreen = true
+	
+	--main.fightscreen.mode = true
+	main.fightscreen.match = true
+	--main.fightscreen.timer = true
+	--main.fightscreen.p1score = true
+	--main.fightscreen.p1wincount = true
+	--main.fightscreen.p2ailevel = true
+	
+	main.matchWins.draw = {0, 0}
+	main.matchWins.simul = {1, 1}
+	main.matchWins.single = {1, 1}
+	main.matchWins.tag = {1, 1}
+	
+	main.storyboard.gameover = true
+	--main.storyboard.credits = true
+end
+
+main.t_itemname.vs100kumite = function()
+	remapInput(1, getLastInputController())
+	remapInput(getLastInputController(), 1)
+	main.motif.challenger = true
+	f_commonCfg()
+	
+	main.orderSelect[1] = true
+	main.orderSelect[2] = true
+	
+	main.teamMenu[1].single = true
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	main.teamMenu[1].turns = true
+	main.teamMenu[1].ratio = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.vs100kumite)
+	setGameMode('vs100kumite')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.vs100kumitecoop = function()
+	main.coop = true
+	main.numSimul = {2, math.min(4, gameOption('Config.Players'))}
+	main.numTag = {2, math.min(4, gameOption('Config.Players'))}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.vs100kumitecoop)
+	setGameMode('vs100kumitecoop')
+	return start.f_selectMode
+end
+
+main.t_itemname.netplayvs100kumitecoop = function()
+	main.coop = true
+	main.numSimul = {2, 2}
+	main.numTag = {2, 2}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.netplayvs100kumitecoop)
+	setGameMode('netplayvs100kumitecoop')
+	return start.f_selectMode
+end
+
+--;===========================================================
+--; start.lua
+--;===========================================================
+--[[
+start.t_makeRoster is a table storing functions returning table data used
+by start.f_makeRoster function, depending on game mode.
+]]
+start.t_makeRoster.vs100kumite = function()
+	local t = {}
+	local t_static = {main.t_randomChars}
+	for i = 1, 100 do --generate ratiomatches style table for 100 matches
+		table.insert(t, {['rmin'] = start.p[2].numChars, ['rmax'] = start.p[2].numChars, ['order'] = 1})
+	end
+	return t, t_static
+end
+start.t_makeRoster.vs100kumitecoop = start.t_makeRoster.vs100kumite
+start.t_makeRoster.netplayvs100kumitecoop = start.t_makeRoster.vs100kumite
