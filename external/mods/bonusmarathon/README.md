57ac3fdd33b8ac8586842eca76e5b55fb1d14154
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-04-20 14:31:36 -0400

    Nightly Update

---
+++ external/mods/bonusmarathon/README.md
@@ -0,0 +1,56 @@
+# Bonus Marathon Mode (I.K.E.M.E.N. GO Module)
+
+***Bonus Marathon*** is a special challenge where player face multiple bonus characters consecutively and tries to beat their previous best score.
+
+Only characters with select.def "bonus = 1" parameter assigned are valid for this mode.
+
+Includes a Co-Op and Netplay variant!
+This mode is detectable by [GameMode](https://github.com/ikemen-engine/Ikemen-GO/wiki/Triggers-(new)#gamemode) trigger as:
+- bonusmarathon
+- bonusmarathoncoop
+- netplaybonusmarathoncoop
+
+## Installation
+
+### 1) Install the module
+Copy the entire `bonusmarathon` directory into:
+`Ikemen_GO/external/mods/`
+
+Ikemen GO will load the module automatically on startup.
+
+### 2) Add the menu item to your screenpack
+Add the below entry to your main `system.def` file, under **`[Title Info]`**, alongside other `menu.itemname.*` entries. Place it where you want it to be grouped/ordered in the menu (grouping rules: https://github.com/ikemen-engine/Ikemen-GO/wiki/Screenpack-features#menus).
+Without this, the mode won't show up in main menu.
+
+```ini
+[Title Info]
+;Bonus Marathon Modes
+menu.itemname.bonusmarathon = "BONUS MARATHON"
+menu.itemname.bonusmarathoncoop = "BONUS MARATHON CO-OP"
+menu.itemname.server.netplaybonusmarathoncoop = "BONUS MARATHON CO-OP"
+````
+
+## Screenpack / `system.def` defaults
+
+The module includes its own `system.def` (next to the script) with **default values for the 720p ikemen1 motif**.
+
+You can leave these defaults in the module or overwrite as your needs.
+
+## `select.def` customization
+
+### Limit the number of matches (optional)
+
+Optionally set a maximum number of matches before Score Attack ends:
+
+```ini
+[Characters]
+; - bonus
+;   IKEMEN feature: Set the paramvalue to 1 to include this character in Bonus Marathon mode.
+;	At least 1 character needs this parameter for the mode to be playable.
+
+[Options]
+;Maximum number of normal and ratio matches to play before game ends in Bonus Marathon mode.
+;Leave it empty to face all bonus characters (the "order" parameter is still respected).
+
+bonusmarathon.maxmatches = 6,1,1,0,0,0,0,0,0,0
+````
---
+++ external/mods/bonusmarathon/bonusmarathon.lua
@@ -0,0 +1,192 @@
+--[[	   				         BONUS MARATHON MODULE
+==================================================================================================
+Author: Cable Dorado 2 (CD2)
+Tested on: I.K.E.M.E.N. GO Engine (Nightly Build - 2026.04.19)
+Description: Based on Boss Rush Module.
+Bonus Marathon Mode is about defeat all opponents that are consider bonuses.
+
+(Includes Co-Op and Netplay Variant)
+
+This mode is detectable by GameMode trigger as: bonusmarathon, bonusmarathoncoop and netplaybonusmarathoncoop.
+Only characters with select.def "bonus = 1" parameter assigned are valid for this mode.
+==================================================================================================
+]]
+
+--;===========================================================
+--; main.lua
+--;===========================================================
+--[[
+main.t_itemname is a table storing functions with general game mode
+configuration (usually ending with start.f_selectMode function call).
+]]
+
+local function f_commonCfg()
+	main.charparam.ai = true
+	main.charparam.music = true
+	main.charparam.rounds = true
+	main.charparam.single = true
+	main.charparam.stage = true
+	main.charparam.time = true
+	
+	--main.luaPath = "external/script/default.lua" --path to script executed by start.f_selectMode()
+	--main.persistLife = true --if life should be maintained after match
+	main.aiRamp = false
+	main.elimination = true
+	--main.dropDefeated = true
+	main.exitSelect = true
+	main.rotationChars = true
+	main.makeRoster = true
+	--main.quickContinue = true --if by default continuing should skip player selection
+	main.rankingCondition = true --if winning (clearing) whole mode is needed for rankings to be saved
+	--main.resetScore = true --if loosing should set score for the next match to lose count
+	--main.roundTime = 99
+	
+	--main.motif.versusscreen = true
+	--main.motif.versusmatchno = true
+	--main.motif.dialogue = true
+	--main.motif.losescreen = true
+	main.motif.winscreen = true
+	--main.motif.victoryscreen = true
+	--main.motif.continuescreen = true
+	main.motif.hiscore = true
+	
+	--main.fightscreen.mode = true
+	--main.fightscreen.match = true
+	--main.fightscreen.timer = true
+	main.fightscreen.p1score = true
+	--main.fightscreen.p1wincount = true
+	--main.fightscreen.p2aiLevel = true
+	
+	main.storyboard.gameover = true
+	--main.storyboard.credits = true
+end
+
+main.t_itemname.bonusmarathon = function()
+	remapInput(1, getLastInputController())
+	remapInput(getLastInputController(), 1)
+	main.motif.challenger = true
+	f_commonCfg()
+	
+	main.orderSelect[1] = true
+	main.orderSelect[2] = true
+
+	main.teamMenu[1].single = true
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	main.teamMenu[1].turns = true
+	main.teamMenu[1].ratio = true
+	
+	main.teamMenu[2].single = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.bonusmarathon)
+	setGameMode('bonusmarathon')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.bonusmarathoncoop = function()
+	main.coop = true
+	main.numSimul = {2, math.min(4, gameOption('Config.Players'))}
+	main.numTag = {2, math.min(4, gameOption('Config.Players'))}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.bonusmarathoncoop)
+	setGameMode('bonusmarathoncoop')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.netplaybonusmarathoncoop = function()
+	main.coop = true
+	main.numSimul = {2, 2}
+	main.numTag = {2, 2}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.netplaybonusmarathoncoop)
+	setGameMode('netplaybonusmarathoncoop')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.bonus = function(t, item)
+	remapInput(1, getLastInputController())
+	remapInput(getLastInputController(), 1)
+	
+	main.charparam.ai = true
+	main.charparam.music = true
+	main.charparam.rounds = true
+	main.charparam.single = true
+	main.charparam.stage = true
+	main.charparam.time = true
+	
+	main.forceChar[2] = {main.t_bonusChars[item]}
+	main.selectMenu[2] = true
+	main.fightscreen.p1score = true
+	
+	main.teamMenu[1].single = true
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	main.teamMenu[1].turns = true
+	main.teamMenu[1].ratio = true
+	
+	main.teamMenu[2].single = true
+	
+	main.motif.challenger = true
+	main.f_setCredits()
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.bonus)
+	setGameMode('bonus')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_bonusmarathonChars = {}
+for _, v in ipairs(main.t_selChars) do
+	if v.bonus ~= nil and v.bonus == 1 then
+		local order = math.max(1, v.order)
+		if main.t_bonusmarathonChars[order] == nil then
+			main.t_bonusmarathonChars[order] = {}
+		end
+		table.insert(main.t_bonusmarathonChars[order], v.char_ref)
+	end
+end
+
+if main.t_selOptions.bonusmarathonmaxmatches == nil or #main.t_selOptions.bonusmarathonmaxmatches == 0 then
+	local size = 1
+	for k, _ in pairs(main.t_bonusmarathonChars) do
+		if k > size then size = k end
+	end
+	main.t_selOptions.bonusmarathonmaxmatches = {}
+	for i = 1, size do
+		table.insert(main.t_selOptions.bonusmarathonmaxmatches, 0)
+	end	
+	for k, v in pairs(main.t_bonusmarathonChars) do
+		main.t_selOptions.bonusmarathonmaxmatches[k] = #v
+	end
+end
+
+--;===========================================================
+--; start.lua
+--;===========================================================
+--[[
+start.t_makeRoster is a table storing functions returning table data used
+by start.f_makeRoster function, depending on game mode.
+]]
+
+start.t_makeRoster.bonusmarathon = function()
+	return start.f_unifySettings(main.t_selOptions.bonusmarathonmaxmatches, main.t_bonusmarathonChars), main.t_bonusmarathonChars
+end
+start.t_makeRoster.bonusmarathoncoop = start.t_makeRoster.bonusmarathon
+start.t_makeRoster.netplaybonusmarathoncoop = start.t_makeRoster.bonusmarathon
+
+if gameOption('Debug.DumpLuaTables') then main.f_printTable(main.t_bonusmarathonChars, "debug/t_bonusmarathonChars.txt") end
