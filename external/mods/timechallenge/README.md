498416c0402add9d77ca20a664d51e076dcb2390
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-04-20 18:59:15 -0400

    Nightly Update

---
+++ external/mods/timechallenge/README.md
@@ -0,0 +1,44 @@
+# Time Challenge Mode Tweaks (I.K.E.M.E.N. GO Module)
+
+Add the following Personal Improvements for [K4thos Time Challenge Module](https://github.com/ikemen-engine/modules/tree/main/external/mods/timechallenge) to make it more faithful to   
+[Super Street Fighter II: The New Challengers](https://www.youtube.com/watch?v=_AEc681QQQ4&list=PLTb7Uia5Yj_Sa9s4uByN4qM7XdS4XJ0gk) which it is based on:
+
+- Enables the "Here Comes a New Challenger" Intermission
+- Set 1 Round to Win
+- Set Infinite Round Time
+- Time Record now is Saved in Ranking only if player wins **(MISSING)**
+- Adds Co-Op and Netplay Variant
+
+> [!NOTE]
+> - **This module overwrites the [original](https://github.com/ikemen-engine/modules/tree/main/external/mods/scorechallenge) Score Challenge module**, so if you're using it, make a backup.
+> 
+> This mode is detectable by [GameMode](https://github.com/ikemen-engine/Ikemen-GO/wiki/Triggers-(new)#gamemode) trigger as:
+>- timechallenge
+>- timechallengecoop
+>- netplaytimechallengecoop
+
+## Installation
+
+### 1) Install the module
+Copy the entire `timechallenge` directory into:
+`Ikemen_GO/external/mods/`
+
+Ikemen GO will load the module automatically on startup.
+
+### 2) Add the menu item to your screenpack
+Add the below entry to your main `system.def` file, under **`[Title Info]`**, alongside other `menu.itemname.*` entries. Place it where you want it to be grouped/ordered in the menu (grouping rules: https://github.com/ikemen-engine/Ikemen-GO/wiki/Screenpack-features#menus).
+Without this, the mode won't show up in main menu.
+
+```ini
+[Title Info]
+;Time Challenge Modes
+menu.itemname.timechallenge = "TIME CHALLENGE"
+menu.itemname.timechallengecoop = "TIME CHALLENGE CO-OP"
+menu.itemname.server.netplaytimechallengecoop = "TIME CHALLENGE CO-OP"
+````
+
+## Screenpack / `system.def` defaults
+
+The module includes its own `system.def` (next to the script) with **default values for the 720p ikemen1 motif**.
+
+You can leave these defaults in the module or overwrite as your needs.
---
+++ external/mods/timechallenge/timechallenge.lua
@@ -0,0 +1,116 @@
+--[[	   					       TIME CHALLENGE MODULE
+=======================================================================================================
+Original Author: K4thos | Edited By: Cable Dorado 2 (CD2)
+Tested on: I.K.E.M.E.N. GO Engine (Nightly Build - 2026.04.20)
+Description: A special challenge where player fight a selected opponent and tries to beat their previous best time.
+Based on Super Street Fighter II: The New Challengers (Sega Mega Drive).
+
+This mode is detectable by GameMode trigger as: timechallenge, timechallengecoop and netplaytimechallengecoop
+=======================================================================================================
+CD2's Tweaks:
+- Enables the "Here Comes a New Challenger" Intermission
+- Set 1 Round to Win
+- Set Infinite Round Time
+- Time Record now is Saved in Ranking if player wins (MISSING)
+- Adds Co-Op and Netplay Variant
+=======================================================================================================
+]]
+
+--;===========================================================
+--; main.lua
+--;===========================================================
+--[[
+main.t_itemname is a table storing functions with general game mode
+configuration (usually ending with start.f_selectMode function call).
+]]
+local function f_commonCfg()
+	main.selectMenu[2] = true
+	main.stageMenu = true
+	main.roundTime = -1 --Infinite Round Time
+	main.elimination = true --if single lose should stop further lua execution
+	main.rankingCondition = true --if winning (clearing) whole mode is needed for rankings to be saved
+	
+	main.motif.hiscore = true
+	main.motif.vsscreen = true
+	main.motif.victoryscreen = true
+	main.motif.winscreen = true
+	main.motif.losescreen = true
+	
+	--main.fightscreen.mode = true
+	main.fightscreen.timer = true
+	--main.fightscreen.p2ailevel = true
+	
+	main.matchWins.draw = {0, 0}
+	main.matchWins.simul = {1, 1}
+	main.matchWins.single = {1, 1}
+	main.matchWins.tag = {1, 1}
+end
+
+main.t_itemname.timechallenge = function()
+	remapInput(1, getLastInputController())
+	remapInput(getLastInputController(), 1)
+	main.motif.challenger = true
+	f_commonCfg()
+	
+	main.orderSelect[1] = true
+	main.orderSelect[2] = true
+	
+	main.teamMenu[1].single = true
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	main.teamMenu[1].turns = true
+	main.teamMenu[1].ratio = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.timechallenge)
+	setGameMode('timechallenge')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.timechallengecoop = function()
+	main.coop = true
+	main.numSimul = {2, math.min(4, gameOption('Config.Players'))}
+	main.numTag = {2, math.min(4, gameOption('Config.Players'))}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.timechallengecoop)
+	setGameMode('timechallengecoop')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.netplaytimechallengecoop = function()
+	main.coop = true
+	main.numSimul = {2, 2}
+	main.numTag = {2, 2}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.netplaytimechallengecoop)
+	setGameMode('netplaytimechallengecoop')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
