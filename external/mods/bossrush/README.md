4361ad9dd09a32b359ff4bc10f423246defb4e68
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-04-20 17:02:27 -0400

    Nightly Update

---
+++ external/mods/bossrush/README.md
@@ -0,0 +1,63 @@
+# Boss Rush Mode Tweaks (I.K.E.M.E.N. GO Module)
+
+Add the following Personal Improvements for [K4thos Boss Rush Module](https://github.com/ikemen-engine/modules/tree/main/external/mods/bossrush):
+
+- VS Screen Restored
+- Enables the "Here Comes a New Challenger" Intermission
+- Adds Co-Op and Netplay Variant
+- main.t_bossChars renamed to main.t_bossRushChars for planned Single Boss Fight variant like Bonus Games Mode
+
+> [!NOTE]
+> - **This module overwrites the [original](https://github.com/ikemen-engine/modules/tree/main/external/mods/bossrush) Boss Rush module**, so if you're using it, make a backup.
+> - **Only characters with select.def "boss = 1" parameter assigned are valid for this mode.**
+> - If boss characters are not detected, the Game Mode will not be playable.
+> 
+> This mode is detectable by [GameMode](https://github.com/ikemen-engine/Ikemen-GO/wiki/Triggers-(new)#gamemode) trigger as:
+>- bossrush
+>- bossrushcoop
+>- netplaybossrushcoop
+
+## Installation
+
+### 1) Install the module
+Copy the entire `bossrush` directory into:
+`Ikemen_GO/external/mods/`
+
+Ikemen GO will load the module automatically on startup.
+
+### 2) Add the menu item to your screenpack
+Add the below entry to your main `system.def` file, under **`[Title Info]`**, alongside other `menu.itemname.*` entries. Place it where you want it to be grouped/ordered in the menu (grouping rules: https://github.com/ikemen-engine/Ikemen-GO/wiki/Screenpack-features#menus).
+Without this, the mode won't show up in main menu.
+
+```ini
+[Title Info]
+;Boss Rush Modes
+menu.itemname.bossrush = "BOSS RUSH"
+menu.itemname.bossrushcoop = "BOSS RUSH CO-OP"
+menu.itemname.server.netplaybossrushcoop = "BOSS RUSH CO-OP"
+````
+
+## Screenpack / `system.def` defaults
+
+The module includes its own `system.def` (next to the script) with **default values for the 720p ikemen1 motif**.
+
+You can leave these defaults in the module or overwrite as your needs.
+
+## `select.def` customization
+
+### Limit the number of matches (optional)
+
+Optionally set a maximum number of matches before Boss Rush ends:
+
+```ini
+[Characters]
+; - boss
+;   IKEMEN feature: Set the paramvalue to 1 to include this character in "Boss Rush" mode.
+;   At least 1 character needs this parameter for the mode to be playable.
+
+[Options]
+;Maximum number of normal and ratio matches to fight before game ends in Boss Rush mode.
+;Leave it empty to fight all boss characters (the "order" parameter is still respected).
+
+bossrush.maxmatches = 6,1,1,0,0,0,0,0,0,0
+````
---
+++ external/mods/bossrush/bossrush.lua
@@ -0,0 +1,220 @@
+--[[	   					       BOSS RUSH MODULE
+=======================================================================================================
+Original Author: K4thos | Edited By: Cable Dorado 2 (CD2)
+Tested on: I.K.E.M.E.N. GO Engine (Nightly Build - 2026.04.20)
+Description: A special challenge where player fight multiple bosses consecutively.
+Beating all bosses clears the mode.
+
+This mode is detectable by GameMode trigger as: bossrush, bossrushcoop and netplaybossrushcoop.
+Only characters with select.def "boss = 1" parameter assigned are valid for this mode.
+=======================================================================================================
+CD2's Tweaks:
+- VS Screen Restored
+- Enables the "Here Comes a New Challenger" Intermission
+- Adds Co-Op and Netplay Variant
+- main.t_bossChars renamed to main.t_bossRushChars for planned Single Boss Fight variant like Bonus Games Mode
+
+TODO: Add Single Boss Fight game mode (defeat a boss character selected) detectable by GameMode trigger as boss.
+=======================================================================================================
+]]
+
+--;===========================================================
+--; main.lua
+--;===========================================================
+--[[
+main.t_itemname is a table storing functions with general game mode
+configuration (usually ending with start.f_selectMode function call).
+]]
+
+local function f_commonCfg()
+	main.charparam.ai = true
+	main.charparam.music = true
+	main.charparam.rounds = true
+	main.charparam.single = true
+	main.charparam.stage = true
+	main.charparam.time = true
+	
+	--main.luaPath = "external/script/default.lua" --path to script executed by start.f_selectMode()
+	--main.persistLife = true --if life should be maintained after match
+	main.aiRamp = false
+	main.elimination = true
+	--main.dropDefeated = true
+	main.exitSelect = true
+	main.rotationChars = true
+	main.makeRoster = true
+	--main.quickContinue = true --if by default continuing should skip player selection
+	--main.rankingCondition = true --if winning (clearing) whole mode is needed for rankings to be saved
+	--main.resetScore = true --if loosing should set score for the next match to lose count
+	--main.roundTime = 99
+	
+	main.motif.versusscreen = true
+	--main.motif.versusmatchno = true
+	--main.motif.dialogue = true
+	main.motif.losescreen = true
+	main.motif.winscreen = true
+	main.motif.victoryscreen = true
+	--main.motif.continuescreen = true
+	main.motif.hiscore = true
+	
+	--main.fightscreen.mode = true
+	--main.fightscreen.match = true
+	--main.fightscreen.timer = true
+	main.fightscreen.p1score = true
+	--main.fightscreen.p1wincount = true
+	--main.fightscreen.p2aiLevel = true
+	
+	main.storyboard.gameover = true
+	--main.storyboard.credits = true
+end
+
+main.t_itemname.bossrush = function()
+	remapInput(1, getLastInputController())
+	remapInput(getLastInputController(), 1)
+	main.motif.challenger = true
+	f_commonCfg()
+	
+	main.orderSelect[1] = true
+	main.orderSelect[2] = true
+	
+	main.teamMenu[1].single = true
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	main.teamMenu[1].turns = true
+	main.teamMenu[1].ratio = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.bossrush)
+	setGameMode('bossrush')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.bossrushcoop = function()
+	main.coop = true
+	main.numSimul = {2, math.min(4, gameOption('Config.Players'))}
+	main.numTag = {2, math.min(4, gameOption('Config.Players'))}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.bossrushcoop)
+	setGameMode('bossrushcoop')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.netplaybossrushcoop = function()
+	main.coop = true
+	main.numSimul = {2, 2}
+	main.numTag = {2, 2}
+	f_commonCfg()
+	
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	
+	main.teamMenu[2].single = true
+	main.teamMenu[2].simul = true
+	main.teamMenu[2].tag = true
+	main.teamMenu[2].turns = true
+	main.teamMenu[2].ratio = true
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.netplaybossrushcoop)
+	setGameMode('netplaybossrushcoop')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_itemname.boss = function() --W.I.P
+	remapInput(1, getLastInputController())
+	remapInput(getLastInputController(), 1)
+	main.selectMenu[2] = true
+	main.motif.victoryscreen = true
+	
+	main.charparam.ai = true
+	main.charparam.music = true
+	main.charparam.rounds = true
+	main.charparam.single = true
+	main.charparam.stage = true
+	main.charparam.time = true
+	
+	main.orderSelect[1] = true
+	main.orderSelect[2] = true
+	
+	main.teamMenu[1].single = true
+	main.teamMenu[1].simul = true
+	main.teamMenu[1].tag = true
+	main.teamMenu[1].turns = true
+	main.teamMenu[1].ratio = true
+	
+	main.teamMenu[2].single = true
+	main.forceChar[2] = {main.t_bossChars[item]}
+	
+	main.motif.challenger = true
+	main.f_setCredits()
+	
+	textImgSetText(motif.select_info.title.TextSpriteData, motif.select_info.title.text.boss)
+	setGameMode('boss')
+	hook.run("main.t_itemname")
+	return start.f_selectMode
+end
+
+main.t_bossChars = {}
+main.t_bossRushChars = {}
+for _, v in ipairs(main.t_selChars) do
+	if v.boss ~= nil and v.boss == 1 then
+		if main.t_bossChars == nil then
+			main.t_bossChars = {}
+		end
+		local order = math.max(1, v.order)
+		if main.t_bossRushChars[order] == nil then
+			main.t_bossRushChars[order] = {}
+		end
+		table.insert(main.t_bossChars, v.char_ref)
+		table.insert(main.t_bossRushChars[order], v.char_ref)
+	end
+end
+
+if main.t_selOptions.bossrushmaxmatches == nil or #main.t_selOptions.bossrushmaxmatches == 0 then
+	local size = 1
+	for k, _ in pairs(main.t_bossRushChars) do
+		if k > size then size = k end
+	end
+	main.t_selOptions.bossrushmaxmatches = {}
+	for i = 1, size do
+		table.insert(main.t_selOptions.bossrushmaxmatches, 0)
+	end	
+	for k, v in pairs(main.t_bossRushChars) do
+		main.t_selOptions.bossrushmaxmatches[k] = #v
+	end
+end
+
+--;===========================================================
+--; start.lua
+--;===========================================================
+--[[
+start.t_makeRoster is a table storing functions returning table data used
+by start.f_makeRoster function, depending on game mode.
+]]
+
+start.t_makeRoster.bossrush = function()
+	return start.f_unifySettings(main.t_selOptions.bossrushmaxmatches, main.t_bossRushChars), main.t_bossRushChars
+end
+start.t_makeRoster.bossrushcoop = start.t_makeRoster.bossrush
+start.t_makeRoster.netplaybossrushcoop = start.t_makeRoster.bossrush
+
+if gameOption('Debug.DumpLuaTables') then
+	main.f_printTable(main.t_bossChars, "debug/t_bossChars.txt")
+	main.f_printTable(main.t_bossRushChars, "debug/t_bossRushChars.txt")
+end
