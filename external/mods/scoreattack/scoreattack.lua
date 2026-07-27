69ec3f4b66af2f965fbdcd13b2948e6a7019d33b
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-07-27 11:33:30 -0400

    Compatibility test with v1.0.0-rc.1

--- external/mods/scoreattack/scoreattack.lua
+++ external/mods/scoreattack/scoreattack.lua
@@ -1,7 +1,7 @@
 --[[	   					       SCORE ATTACK MODULE
 =======================================================================================================
 Author: Cable Dorado 2 (CD2)
-Tested on: I.K.E.M.E.N. GO Engine (Nightly Build - 2026.04.20)
+Tested on: I.K.E.M.E.N. GO Engine (v1.0.0-rc.1)
 Description: Implements Score Attack game mode (defeat opponents beating previous score record).
 Includes a Co-Op and Netplay variant.
 
