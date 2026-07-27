245aa42d2526d3b7584b427a141178ad5237f03d
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-07-27 11:35:29 -0400

    Compatibility test with v1.0.0-rc.1

--- external/mods/timeattack/timeattack.lua
+++ external/mods/timeattack/timeattack.lua
@@ -1,7 +1,7 @@
 --[[	   							TIME ATTACK TWEAKS
 =======================================================================================================
 Author: Cable Dorado 2 (CD2)
-Tested on: I.K.E.M.E.N. GO Engine (Nightly Build - 2026.04.20)
+Tested on: I.K.E.M.E.N. GO Engine (v1.0.0-rc.1)
 Description: Adds personal improvements to make Time Attack more faithful to Commercial Games.
 
 The new Co-op modes are detectable by GameMode trigger as: timeattackcoop and netplaytimeattackcoop
