d35d5d00fac8a08ec50a5ead89946404890de05e
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-09-05 21:49:58 -0400

    Compatibility test with v1.0.0-rc.5

--- external/mods/bonusmarathon/bonusmarathon.lua
+++ external/mods/bonusmarathon/bonusmarathon.lua
@@ -1,7 +1,7 @@
 --[[	   				         BONUS MARATHON MODULE
 ==================================================================================================
 Author: Cable Dorado 2 (CD2)
-Tested on: I.K.E.M.E.N. GO Engine (v1.0.0-rc.3)
+Tested on: I.K.E.M.E.N. GO Engine (v1.0.0-rc.5)
 Description: Based on Boss Rush Module.
 Bonus Marathon Mode is about defeat all opponents that are consider bonuses.
 
