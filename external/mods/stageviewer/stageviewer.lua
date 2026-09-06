36b454310b23bf7998d1922449f4a7bf31463f8a
CD2 <18058378+CableDorado2@users.noreply.github.com>
2026-09-05 21:53:16 -0400

    Compatibility test with v1.0.0-rc.5

--- external/mods/stageviewer/stageviewer.lua
+++ external/mods/stageviewer/stageviewer.lua
@@ -1,7 +1,7 @@
 --[[	   					       STAGE VIEWER MODULE
 =======================================================================================================
 Author: Cable Dorado 2 (CD2) & Yoshin222
-Tested on: I.K.E.M.E.N. GO Engine (v1.0.0-rc.3)
+Tested on: I.K.E.M.E.N. GO Engine (v1.0.0-rc.5)
 Description: Adds a Stage Viewer Game Mode, based on Yoshin222's Stage Viewer Character.
 
 This mode is detectable by GameMode trigger as: stageviewer
